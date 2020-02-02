0499a02ed288e12680bd682fc596ad407fa96c7f
E-karim94 <56731757+E-karim94@users.noreply.github.com>
2020-02-02 20:57:33 +0000

    updated app.js

--- data/courses.js
+++ data/courses.js
@@ -2,16 +2,11 @@ const vuedisplaycontent = new Vue({
     el:'#content',
     data:{
       courses: [ { 'slug':'image1','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
-      {'slug':'image', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
-      {'slug':'hhdf', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
-      {'slug':'ikjg', 'topic': 'math', 'provider':'gerns', 'location': 'golders green', 'price': 120 },
-      { 'slug':'kllggg','topic': 'english','provider':'oil',  'location': 'hendon', 'price': 110 },
-      {'slug':'gtfd', 'topic': 'english', 'provider':'philips', 'location': 'colindale', 'price': 90 },
-      {'slug':'olkj', 'topic': 'english', 'provider':'sony', 'location': 'brent cross', 'price': 90 },
-      { 'slug':'kjjdfg','topic': 'english', 'provider':'samsung', 'location': 'golders green', 'price': 130 },
-      { 'slug':'mgjjdh','topic': 'piano', 'provider':'dell', 'location': 'hendon', 'price': 120 },
-      { 'slug':'nhdhkfj','topic': 'piano', 'provider':'apple', 'location': 'golders green', 'price': 140 }
-     ]
+      { 'slug':'image1','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
+      {'slug':'image2', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
+      {'slug':'image3', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
+      ]
+     
       
     
     }}
