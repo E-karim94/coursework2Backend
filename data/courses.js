b0464f2a315d0fd646ffff3ac77e87373a37490c
E-karim94 <56731757+E-karim94@users.noreply.github.com>
2020-02-02 22:45:45 +0000

    udated index and courses.js

--- data/courses.js
+++ data/courses.js
@@ -2,9 +2,9 @@ const vuedisplaycontent = new Vue({
     el:'#content',
     data:{
       courses: [ { 'slug':'image1','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
-      { 'slug':'image1','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
-      {'slug':'image2', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
-      {'slug':'image3', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
+      { 'slug':'image2','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
+      {'slug':'image4', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
+      {'slug':'image6', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
       ]
      
       
