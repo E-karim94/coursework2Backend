e932b6f209843fdf37cb5bc8656215941fe6d7e2
E-karim94 <56731757+E-karim94@users.noreply.github.com>
2020-02-02 19:37:39 +0000

    updated sw.js

--- sw.js
+++ sw.js
@@ -13,6 +13,23 @@ var appShellFiles = [
   
 ];
 
+ var courses = [ { 'slug':'image1','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
+{'slug':'image', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
+{'slug':'hhdf', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
+{'slug':'ikjg', 'topic': 'math', 'provider':'gerns', 'location': 'golders green', 'price': 120 },
+{ 'slug':'kllggg','topic': 'english','provider':'oil',  'location': 'hendon', 'price': 110 },
+{'slug':'gtfd', 'topic': 'english', 'provider':'philips', 'location': 'colindale', 'price': 90 },
+{'slug':'olkj', 'topic': 'english', 'provider':'sony', 'location': 'brent cross', 'price': 90 },
+{ 'slug':'kjjdfg','topic': 'english', 'provider':'samsung', 'location': 'golders green', 'price': 130 },
+{ 'slug':'mgjjdh','topic': 'piano', 'provider':'dell', 'location': 'hendon', 'price': 120 },
+{ 'slug':'nhdhkfj','topic': 'piano', 'provider':'apple', 'location': 'golders green', 'price': 140 }
+]
+
+var coursesImages = [];
+for (var i = 0 ; i <courses.length ; i++){
+    gamesImages.push('data/img/'+courses[i].slug+'.jpg');
+}var contentToCache = appShellFiles.concat(coursesImages);
+
 self.addEventListener('install',(e)=>{
     console.log('[Service Worker] Install');
     e.waitUntil(
