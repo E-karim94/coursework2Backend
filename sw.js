d0d7003f1c1bd69224690302e61f5bec067f4ac2
E-karim94 <56731757+E-karim94@users.noreply.github.com>
2020-02-02 19:59:39 +0000

    updated app.js

--- sw.js
+++ sw.js
@@ -1,34 +1,24 @@
 var cacheName ='coursework2-v1'; 
+
 var appShellFiles = [
     '/coursework2Backend/coursework2/',
     '/coursework2Backend/coursework2/index.html',
     '/coursework2Backend/coursework2/app.js',
     '/coursework2Backend/coursework2/home.css',
-    '/coursework2Backend/coursework2/data/images/image1.jpg ',
-    '/coursework2Backend/coursework2/data/images/image2.jpg ',
-    '/coursework2Backend/coursework2/data/images/image3.jpg ',
-    '/coursework2Backend/coursework2/data/images/image4.jpg ',
-    '/coursework2Backend/coursework2/data/images/image5.jpg ',
-    '/coursework2Backend/coursework2/data/images/image6.jpg ',
+   
   
 ];
 
  var courses = [ { 'slug':'image1','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
-{'slug':'image', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
-{'slug':'hhdf', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
-{'slug':'ikjg', 'topic': 'math', 'provider':'gerns', 'location': 'golders green', 'price': 120 },
-{ 'slug':'kllggg','topic': 'english','provider':'oil',  'location': 'hendon', 'price': 110 },
-{'slug':'gtfd', 'topic': 'english', 'provider':'philips', 'location': 'colindale', 'price': 90 },
-{'slug':'olkj', 'topic': 'english', 'provider':'sony', 'location': 'brent cross', 'price': 90 },
-{ 'slug':'kjjdfg','topic': 'english', 'provider':'samsung', 'location': 'golders green', 'price': 130 },
-{ 'slug':'mgjjdh','topic': 'piano', 'provider':'dell', 'location': 'hendon', 'price': 120 },
-{ 'slug':'nhdhkfj','topic': 'piano', 'provider':'apple', 'location': 'golders green', 'price': 140 }
+{'slug':'image2', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
+{'slug':'image3', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
 ]
 
 var coursesImages = [];
 for (var i = 0 ; i <courses.length ; i++){
     coursesImages.push('data/imgages/'+courses[i].slug+'.jpg');
-}var contentToCache = appShellFiles.concat(coursesImages);
+}
+var contentToCache = appShellFiles.concat(coursesImages);
 
 self.addEventListener('install',(e)=>{
     console.log('[Service Worker] Install');
