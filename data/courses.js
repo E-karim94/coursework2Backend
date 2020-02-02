6350c2fda2724fc48f335635f1d39d80407d6600
E-karim94 <56731757+E-karim94@users.noreply.github.com>
2020-02-02 23:21:46 +0000

    udated index and courses.js

--- data/courses.js
+++ data/courses.js
@@ -1,10 +1,16 @@
 const vuedisplaycontent = new Vue({
     el:'#content',
     data:{
-      courses: [ { 'slug':'image1','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
-      { 'slug':'image2','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
-      {'slug':'image4', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
-      {'slug':'image6', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
+      courses: [ { 'slug':'image1','topic': 'kids football', 'provider':'mike laley', 'location': 'hendon', 'price': 100 },
+      { 'slug':'image2','topic': 'Dancing', 'provider':'micheal promt', 'location': 'Greater London', 'price': 140 },
+      {'slug':'image3', 'topic': 'Sitch','provider':'Mary Bow',  'location': 'Colindale', 'price': 80 },
+      {'slug':'image4', 'topic': 'Sport of under 4','provider':'Lehla Megan',  'location': 'Brent Cross', 'price': 90 },
+      {'slug':'image5', 'topic': 'Skateboarding','provider':'Brian Sama',  'location': 'Hackney', 'price': 200 },
+      {'slug':'image6', 'topic': 'Robots for kids','provider':'Priti Patel',  'location': 'Downing Street', 'price': 190 },
+      {'slug':'image7', 'topic': 'Feel at Home','provider':'Nana Kwami',  'location': 'Ilford ', 'price': 299 },
+      {'slug':'image8', 'topic': 'Bounce','provider':'Kean Lee',  'location': 'Tottenham', 'price': 179 },
+      {'slug':'image9', 'topic': 'Multi Sport','provider':'Sata Rahmani',  'location': 'Seven sisters', 'price': 90 },
+      {'slug':'image10', 'topic': 'Coaching','provider':'Garry Williams',  'location': 'Bruce Grove', 'price': 245 },
       ]
      
       
