aae6878da8e5f2030211f6eb5e5f21322869abaf
E-karim94 <56731757+E-karim94@users.noreply.github.com>
2020-02-02 18:23:28 +0000

    service worker and notification updated

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-const express = require('express')
-const app = express()
-// load bodyParser module for json payload parsing
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-// connect to MongoDB
-const MongoClient = require('mongodb').MongoClient;
-let db;
-MongoClient.connect('mongodb://127.0.0.1:27017/', (err, client) => {
-db = client.db('coursework')
-})
-// get the collection name
-app.param('collectionName', (req, res, next, collectionName) => {
-req.collection = db.collection(collectionName)
-// console.log('collection name:', req.collection)
-return next()
-})
+if('serviceWorker'in navigator)
+{navigator.serviceWorker.register('/pwa-examples/js13kpwa/sw.js');};
 
 
+var button = document.getElementById("notifications");
+button.addEventListener('click',function(e){
+Notification.requestPermission().then(function(result){
+    if(result==='granted'){randomNotification();
+    }}
+    );
+});
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-})
+var courses = [ { 'slug':'image','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
+{'slug':'image', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
+{'slug':'hhdf', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
+{'slug':'ikjg', 'topic': 'math', 'provider':'gerns', 'location': 'golders green', 'price': 120 },
+{ 'slug':'kllggg','topic': 'english','provider':'oil',  'location': 'hendon', 'price': 110 },
+{'slug':'gtfd', 'topic': 'english', 'provider':'philips', 'location': 'colindale', 'price': 90 },
+{'slug':'olkj', 'topic': 'english', 'provider':'sony', 'location': 'brent cross', 'price': 90 },
+{ 'slug':'kjjdfg','topic': 'english', 'provider':'samsung', 'location': 'golders green', 'price': 130 },
+{ 'slug':'mgjjdh','topic': 'piano', 'provider':'dell', 'location': 'hendon', 'price': 120 },
+{ 'slug':'nhdhkfj','topic': 'piano', 'provider':'apple', 'location': 'golders green', 'price': 140 }
+]
 
 
+function randomNotification(){
+    var randomItem = Math.floor(Math.random()*courses.length);
+    var notifTitle = courses[randomItem].topic;
+    var notifBody='Created by '+courses[randomItem].provider+'.';
+    var notifImg='data/img/'+courses[randomItem].slug+'.jpg';
+    var options = {body:notifBody,
+        icon:notifImg
+    }
+    var notif = new Notification(notifTitle,options);
+    setTimeout(randomNotification,300000000);
+}
 
 
 
