4d158dc193d5d24f6421474015fcd8861ab50e2b
E-karim94 <56731757+E-karim94@users.noreply.github.com>
2020-02-03 13:50:05 +0000

    udated app.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var appShellFiles = [
     '/coursework2Backend/index.html',
     '/coursework2Backend/app.js',
     '/coursework2Backend/home.css',
-    '/coursework2Backend/data/images/image1.jpg ',
+    '/coursework2Backend/data/images/image2.jpg',
     '/coursework2Backend/data/images/image2.jpg ',
     '/coursework2Backend/data/images/image3.jpg ',
     '/coursework2Backend/data/images/image4.jpg ',
@@ -33,3 +33,17 @@ self.addEventListener('install',(e)=>{
             );
         }
  );
+
+ /*self.addEventListener('fetch',function(e){
+     e.respondWith(
+         caches.match(e.request).then(function(r){
+             console.log('[Service Worker] Fetching resource: '+e.request.url);
+             return r || fetch(e.request).then(function (response) {
+                 return caches.open(cacheName).then(function(cache)
+                 {
+                console.log('[Service Worker] Caching new resource: '+e.request.url);
+                cache.put(e.request,response.clone());returnresponse;
+            });
+        });
+    }));
+});*/
