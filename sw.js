ddf090b961369215106ad3e70840920168b03dbe
E-karim94 <56731757+E-karim94@users.noreply.github.com>
2020-02-03 00:53:24 +0000

    udated app.js

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 var cacheName ='coursework2-v1'; 
 
 var appShellFiles = [
-    '/coursework2/',
-    '/coursework2/index.html',
-    '/coursework2/app.js',
-    '/coursework2/home.css',
-    '/coursework2/data/images/image1.jpg ',
-    '/coursework2/data/images/image2.jpg ',
-    '/coursework2/data/images/image3.jpg ',
-    '/coursework2/data/images/image4.jpg ',
-    '/coursework2/data/images/image5.jpg ',
-    '/coursework2/data/images/image6.jpg ',
-    '/coursework2/data/images/image7.jpg ',
-    '/coursework2/data/images/image8.jpg ',
-    '/coursework2/data/images/image9.jpg ',
-    '/coursework2/data/images/image10.jpg ',
+    '/coursework2Backend/coursework2/',
+    '/coursework2Backend/coursework2/index.html',
+    '/coursework2Backend/coursework2/app.js',
+    '/coursework2Backend/coursework2/home.css',
+    '/coursework2Backend/coursework2/data/images/image1.jpg ',
+    '/coursework2Backend/coursework2/data/images/image2.jpg ',
+    '/coursework2Backend/coursework2/data/images/image3.jpg ',
+    '/coursework2Backend/coursework2/data/images/image4.jpg ',
+    '/coursework2Backend/coursework2/data/images/image5.jpg ',
+    '/coursework2Backend/coursework2/data/images/image6.jpg ',
+    '/coursework2Backend/coursework2/data/images/image7.jpg ',
+    '/coursework2Backend/coursework2/data/images/image8.jpg ',
+    '/coursework2Backend/coursework2/data/images/image9.jpg ',
+    '/coursework2Backend/coursework2/data/images/image10.jpg ',
    
   
 ];
