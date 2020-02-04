1cadbaef6d4187df6cf9bd32d929084c6fa82192
E-karim94 <56731757+E-karim94@users.noreply.github.com>
2020-02-04 21:58:59 +0000

    newly updated files

--- app.js
+++ app.js
@@ -7,21 +7,23 @@
 var button = document.getElementById("notifications");
 button.addEventListener('click',function(e){
 Notification.requestPermission().then(function(result){
-    if(result==='granted'){randomNotification();
+    if(result==='granted'
+    ){
+        randomNotification();
     }}
     );
 });
 
-var courses = [ { 'slug':'image','topic': 'math', 'provider':'mike', 'location': 'hendon', 'price': 100 },
-{'slug':'image', 'topic': 'math','provider':'deen',  'location': 'colindale', 'price': 80 },
-{'slug':'hhdf', 'topic': 'math','provider':'solo',  'location': 'brent cross', 'price': 90 },
-{'slug':'ikjg', 'topic': 'math', 'provider':'gerns', 'location': 'golders green', 'price': 120 },
-{ 'slug':'kllggg','topic': 'english','provider':'oil',  'location': 'hendon', 'price': 110 },
-{'slug':'gtfd', 'topic': 'english', 'provider':'philips', 'location': 'colindale', 'price': 90 },
-{'slug':'olkj', 'topic': 'english', 'provider':'sony', 'location': 'brent cross', 'price': 90 },
-{ 'slug':'kjjdfg','topic': 'english', 'provider':'samsung', 'location': 'golders green', 'price': 130 },
-{ 'slug':'mgjjdh','topic': 'piano', 'provider':'dell', 'location': 'hendon', 'price': 120 },
-{ 'slug':'nhdhkfj','topic': 'piano', 'provider':'apple', 'location': 'golders green', 'price': 140 }
+var courses =  [ { 'slug':'lego','topic': 'Lego', 'provider':'mike laley', 'location': 'hendon', 'price': 100 },
+{ 'slug':'image2','topic': 'Dancing', 'provider':'micheal promt', 'location': 'Greater London', 'price': 140 },
+{'slug':'image3', 'topic': 'Sitch','provider':'Mary Bow',  'location': 'Colindale', 'price': 80 },
+{'slug':'image4', 'topic': 'Sport of under 4','provider':'Lehla Megan',  'location': 'Brent Cross', 'price': 90 },
+{'slug':'image5', 'topic': 'Skateboarding','provider':'Brian Sama',  'location': 'Hackney', 'price': 200 },
+{'slug':'image6', 'topic': 'Robots for kids','provider':'Priti Patel',  'location': 'Downing Street', 'price': 190 },
+{'slug':'image7', 'topic': 'Feel at Home','provider':'Nana Kwami',  'location': 'Ilford ', 'price': 299 },
+{'slug':'image8', 'topic': 'Bounce','provider':'Kean Lee',  'location': 'Tottenham', 'price': 179 },
+{'slug':'image9', 'topic': 'Multi Sport','provider':'Sata Rahmani',  'location': 'Seven sisters', 'price': 90 },
+{'slug':'image10', 'topic': 'Coaching','provider':'Garry Williams',  'location': 'Bruce Grove', 'price': 245 },
 ]
 
 
@@ -29,13 +31,25 @@ function randomNotification(){
     var randomItem = Math.floor(Math.random()*courses.length);
     var notifTitle = courses[randomItem].topic;
     var notifBody='Created by '+courses[randomItem].provider+'.';
-    var notifImg='data/img/'+courses[randomItem].slug+'.jpg';
+    var notifImg='data/images/'+courses[randomItem].slug+'.jpg';
     var options = {body:notifBody,
         icon:notifImg
     }
     var notif = new Notification(notifTitle,options);
     setTimeout(randomNotification,300000000);
 }
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+    image.setAttribute('src' , image.getAttribute('data-src'));
+    image.onload =() => {
+        image.removeAttribute('data-src');
+    };
+};
+
+imagesToLoad.forEach((img) => { 
+    loadImages(img);
+});
 
 
 
